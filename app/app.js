67a81e288575c293a841a618cef3db60d23dd6e6
Jonatan Östling <jonatan.ostling@gmail.com>
2017-09-04 01:38:42 +0200

    added #

--- app/app.js
+++ app/app.js
@@ -15,43 +15,43 @@ app.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   $routeProvider
-    .when('/', {
+    .when('#/', {
       controller: 'navController',
       templateUrl: 'app/partials/home/home.html'
     })
-    .when('/store', {
+    .when('#/store', {
       controller: 'navController',
       templateUrl: 'app/partials/store/store.html'
     })
-    .when('/band', {
+    .when('#/band', {
       controller: 'navController',
       templateUrl: 'app/partials/band/band.html'
     })
-    .when('/renovering-kok', {
+    .when('#/renovering-kok', {
       controller: 'navController',
       templateUrl: 'app/partials/band/renovering-kok.html'
     })
-    .when('/renovering-badrum', {
+    .when('#/renovering-badrum', {
       controller: 'navController',
       templateUrl: 'app/partials/band/renovering-badrum.html'
     })
-    .when('/renovering-snickeri', {
+    .when('#/renovering-snickeri', {
       controller: 'navController',
       templateUrl: 'app/partials/band/renovering-snickeri.html'
     })
-    .when('/renovering-total', {
+    .when('#/renovering-total', {
       controller: 'navController',
       templateUrl: 'app/partials/band/renovering-total.html'
     })
-    .when('/om-oss', {
+    .when('#/om-oss', {
       controller: 'navController',
       templateUrl: 'app/partials/about/about.html'
     })
-    .when('/gallery-live', {
+    .when('#/gallery-live', {
       controller: 'navController',
       templateUrl: 'app/partials/gallery/gallery-live.html'
     })
-    .when('/gallery-promo', {
+    .when('#/gallery-promo', {
       controller: 'navController',
       templateUrl: 'app/partials/gallery/gallery-promo.html'
     })
@@ -63,29 +63,13 @@ app.config(['$routeProvider', '$locationProvider',
     //   controller: 'navController',
     //   templateUrl: 'app/partials/gallery/gallery-snickeri.html'
     // })
-    .when('/contact', {
+    .when('#/contact', {
       controller: 'navController',
       templateUrl: 'app/partials/contact/contact.html'
     })
-    .when('/kontakt', {
+    .when('#/kontakt', {
       controller: 'navController',
       templateUrl: 'app/partials/contact/contact.html'
     })
     .otherwise({ redirectTo: '/' });
 }]);
-// var controllers = {};
-// controllers.searchController = function($scope) {
-//   $scope.books = [
-//     { name: 'J.R.R. Tolkien', title: 'The Hobbit', img: 'img/books/hobbit.jpg'},
-//     { name: 'Harper Lee', title: 'To Kill A Mockingbird', img: 'img/books/mockingbird.jpg'},
-//     { name: 'Scott Fitzgerald', title: 'The Great Gatsby', img: 'img/books/gatsby.jpg'},
-//     { name: 'Joseph Heller', title: 'Catch-22', img: 'img/books/22.jpg'},
-//     { name: 'Anthony Burgess', title: 'A Clockwork Orange', img: 'img/books/clockwork.jpg'},
-//     { name: 'Dr. Seuss', title: 'The Cat in the Hat', img: 'img/books/cat.png'},
-//     { name: 'Chris Mould', title: 'Something Wickedly Weird', img: 'img/books/wicked.jpg'},
-//     { name: 'George R.R. Martin', title: 'A Game of Thrones', img: 'img/books/thrones.jpg'},
-//     { name: 'J.D. Salinger', title: 'The Catcher in the Rye', img: 'img/books/rye.jpg' }
-//   ];
-// };
-//
-// app.controller(controllers);
