485fdb12da7c194c154d2491c24a65f3a62c0e9d
Jonatan Östling <jonatan.ostling@gmail.com>
2017-09-04 00:23:11 +0200

    test old website

--- app/controllers/navController.js
+++
@@ -1,7 +0,0 @@
-app.controller('navController', ['$scope', '$location', function($scope, $location) {
-
-  $scope.isActive = function(destination) {
-    return destination === $location.path();
-  };
-
-}]);
